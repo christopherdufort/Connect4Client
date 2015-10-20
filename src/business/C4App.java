3ece927a9c0b71a6f4580481dad403be21eaf783
Christopher <dufort.christopher@gmail.com>
2015-10-20 18:27:43 -0400

    Version 0.0.1

---
+++ src/business/C4App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package business;
+
+/**
+ * @author Christopher, Elliot, Nader
+ *
+ */
+public class C4App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
