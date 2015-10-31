daf5e0289bc8ba6c3363f3e69100c98fd1aea337
Christopher <dufort.christopher@gmail.com>
2015-10-30 22:44:04 -0400

    Class Name change and comments.

--- src/business/C4ClientAppFX.java
+++ src/business/C4ClientAppFX.java
@@ -18,13 +18,18 @@ import java.util.Optional;
 import controller.ClientGameController;
 
 /**
- * Basic class for starting a JavaFX application.
+ * C4ClientAppFX is the starting point for the Connect 4 Client application.
+ * This class contain the main method which is the starting program.
+ * As the client is GUI based this class creates and starts the GUI.
+ * 
  * 
  * @author Christopher Dufort
  * @author Elliot Wu
  * @author Nader Baydoun
+ * 
+ * @version Java 1.8
  */
-public class MainAppFX extends Application 
+public class C4ClientAppFX extends Application 
 {
     // The primary window or frame of this application
     private Stage primaryStage;
@@ -33,28 +38,33 @@ public class MainAppFX extends Application
     
     ClientGameController controller;
     
-
     /**
      * Constructor for the FX window.
      */
-    public MainAppFX() 
+    public C4ClientAppFX() 
     {
         super();
         serverHost = "";
     }
    
+    /**
+     * The application starts here, this main method will launch the GUI component of this application.
+     * 
+	 * @param args
+	 *            Command line arguments that may be accepted when running.
+     */
     public static void main(String[] args) 
     {
+        //implicit call to init() and start(javafx.stage.Stage)
         launch(args);
-        //implicit call to init()
-        //implicit call to start(javafx.stage.Stage)
         System.exit(0);
     }
     
     /**
-     * The application starts here
-     *
-     * @param primaryStage The Stage that represents the main Java FX pane.
+     * The start method is used to create and show the GUI component of this application.
+     * 
+     * @param primaryStage 
+     * 			The Stage that represents the main Java FX pane.
      * @throws IOException 
      * @throws UnknownHostException 
      */
@@ -68,6 +78,7 @@ public class MainAppFX extends Application
         // Set the window title
         primaryStage.setTitle("Connect Four");
         
+        //Pop up a dialog box that requests the Server IP Address and will not continue until entered correctly or quit.
 		Optional<String> result;
 		do{
 			result = dialog.showAndWait();
@@ -81,6 +92,7 @@ public class MainAppFX extends Application
 
     /**
      * Load the FXML and bundle, create a Scene and put the Scene on Stage.
+     * The Client Game controller associated with this class is instaciated alongside the creation of the GUI
      *
      * Using this approach allows you to use loader.getController() to get a
      * reference to the fxml's controller should you need to pass data to it.
@@ -99,7 +111,7 @@ public class MainAppFX extends Application
             FXMLLoader loader = new FXMLLoader();
             
             // Set the location of the fxml file in the FXMLLoader
-            loader.setLocation(MainAppFX.class.getResource("Board.fxml")); 
+            loader.setLocation(C4ClientAppFX.class.getResource("Board.fxml")); 
             
             // Parent is the base class for all nodes that have children in the
             // scene graph such as AnchorPane and most other containers
@@ -109,15 +121,13 @@ public class MainAppFX extends Application
             
             // Load the parent into a Scene
             Scene scene = new Scene(parent);
-                 
-                        
+                                   
             //Creates a list of nodes, each node is a child of the grid-pane, each node representing one of it's cells
             ObservableList<Node> bp = (ObservableList<Node>) parent.getChildrenUnmodifiable();
             
             //Traversing the nested panes to get to the grid-pane that represents the gameboard
             AnchorPane ap = (AnchorPane) bp.get(2);            
-            
-            
+                  
             this.controller.setGridPaneChildren((GridPane) ap.getChildren().get(1));    
             
             this.controller.setAnchorPane(ap);
